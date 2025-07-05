043560c4f9498038764755cec37af31c4d37eb36
good-mistake <devillmk88@gmail.com>
2025-07-05 18:17:13 +0330

    c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.scss";
 import { Providers } from "./Providers";
-
+export const metadata = {
+  title: "Entertainment App",
+  icons: {
+    icon: "/assets/logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
