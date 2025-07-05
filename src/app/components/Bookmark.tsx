d1bd34e597cc13f20efc7e994b729b2f6c568f8f
good-mistake <devillmk88@gmail.com>
2025-07-05 19:18:54 +0330

    separated tv from movie bookmarks

--- src/app/components/Bookmark.tsx
+++ src/app/components/Bookmark.tsx
@@ -9,13 +9,20 @@ interface BookmarkProps {
 }
 
 const Bookmark: React.FC<BookmarkProps> = ({ items, onBookmark }) => {
+  const movieBookmark = items.filter(
+    (item) => item.category === "Movie" && item.isBookmarked
+  );
+
+  const tvBookmark = items.filter(
+    (item) => item.category === "TV Series" && item.isBookmarked
+  );
   return (
     <div className="recommended">
       <h1>Bookmarked Movies</h1>
       <div className="recommendedContainer flex w-max grid grid-cols-4 gap-[40px]">
-        {Array.isArray(items) && (
+        {Array.isArray(movieBookmark) && (
           <AnimatePresence>
-            {items.map((e) => {
+            {movieBookmark.map((e) => {
               return (
                 <motion.div
                   key={e.id}
@@ -87,6 +94,85 @@ const Bookmark: React.FC<BookmarkProps> = ({ items, onBookmark }) => {
             })}
           </AnimatePresence>
         )}
+      </div>
+      <div className="tv">
+        <h1>Bookmarked TV Series</h1>
+        <div className="recommendedContainer flex w-max grid grid-cols-4 gap-[40px]">
+          {Array.isArray(tvBookmark) && (
+            <AnimatePresence>
+              {tvBookmark.map((e) => {
+                return (
+                  <motion.div
+                    key={e.id}
+                    layout
+                    exit={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    transition={{ duration: 0.2 }}
+                    className="recommendedItems"
+                  >
+                    <div
+                      style={{
+                        backgroundImage: `url(${e.thumbnail.regular.small})`,
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                      }}
+                    >
+                      <motion.div
+                        onClick={() => onBookmark(e.id)}
+                        whileTap={{ scale: 0.8 }}
+                        transition={{
+                          type: "spring",
+                          stiffness: 300,
+                          damping: 15,
+                        }}
+                        className={`bookmark ${e.isBookmarked ? "active" : ""}`}
+                      >
+                        {e.isBookmarked ? (
+                          <Image
+                            src={`/assets/icon-bookmark-full.svg`}
+                            width={12}
+                            height={14}
+                            alt="bookmark"
+                          />
+                        ) : (
+                          <Image
+                            src={`/assets/icon-bookmark-empty.svg`}
+                            width={12}
+                            height={14}
+                            alt="bookmark"
+                          />
+                        )}
+                      </motion.div>
+                    </div>
+                    <div className="details">
+                      <ul className="flex  text-sm ">
+                        <li>{e.year}</li>
+                        <span></span>
+                        <li>
+                          <Image
+                            src={`${
+                              e.category === "Movie"
+                                ? "/assets/icon-category-movie.svg"
+                                : "/assets/icon-category-tv.svg"
+                            }`}
+                            width={12}
+                            height={12}
+                            alt="bookmark"
+                          />
+                          {e.category}
+                        </li>
+                        <span></span>
+                        <li>{e.rating}</li>
+                      </ul>
+                      <h2>{e.title}</h2>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </AnimatePresence>
+          )}
+        </div>
       </div>
     </div>
   );
